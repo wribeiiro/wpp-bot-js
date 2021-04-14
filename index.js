3bbd1f9c31cfd7ce47648072b85140620ab80bae
Wellisson Ribeiro <welleh10@gmail.com>
2021-04-14 17:42:30 -0300

    some adjusts

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 const puppeteer = require("puppeteer")
 
-const puppeteerParams = {
-    userAgent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36",
-    wppUrl: "https://web.whatsapp.com/",
-    qtyOfMessages: 500,
-    timer: 500,
-    textMessage: "test",
-    classInputTypeMessage: "._2_1wd",
-    contactName: "Leandro",
-    classDivContact: ".OTBsx",
-}
-
 const timer = (time) => {
     return new Promise((resolve) => setTimeout(resolve, time))
 }
@@ -18,25 +7,24 @@ const timer = (time) => {
 const mainBot = async () => {
 
     try {
-
         const browser = await puppeteer.launch({headless: false})
         const page = await browser.newPage()
         
-        await page.setUserAgent(puppeteerParams.userAgent)
-        await page.goto(puppeteerParams.wppUrl)
-        await page.waitForSelector(puppeteerParams.classInputTypeMessage)
-        await timer(puppeteerParams.timer)
+        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36')
+        await page.goto('https://web.whatsapp.com/')
+        await page.waitForSelector('._2_1wd')
+        await timer(5000)
 
-        await page.click(`span[title='${puppeteerParams.contactName}']`)
-        await page.waitForSelector(puppeteerParams.classDivContact)
+        await page.click('span[title="Leandro"]')
+        await page.waitForSelector(".OTBsx")
 
         const editor = await page.$("div[tabindex='-1']")
         await editor.focus()
 
-        for (let i = 0; i < puppeteerParams.qtyOfMessages; i++) {
-            await page.evaluate(() => document.execCommand("insertText", false, `${puppeteerParams.textMessage}-${i}`))
+        for (let i = 0; i < 50; i++) {
+            await page.evaluate(() => document.execCommand("insertText", false, `test++`))
             await page.click("span[data-testid='send']")
-            await timer(puppeteerParams.timer)
+            await timer(1000)
         }
     } catch (e) {
         console.error('deu ruim', e)
